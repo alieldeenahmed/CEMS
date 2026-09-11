fa8b47412214730db1892f0ccec6628375b107a2
alieldeenahmed <alieldeen.ah@gmail.com>
2026-09-11 14:22:16 +0300

    Wire ApplicationDbContext and ASP.NET Identity into DI, add local Postgres connection string, enable authentication middleware

--- Backend/src/CEMS.Api/Program.cs
+++ Backend/src/CEMS.Api/Program.cs
@@ -1,3 +1,8 @@
+using CEMS.Infrastructure.Identity;
+using CEMS.Infrastructure.Persistence;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +10,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("Default")));
+
+builder.Services
+    .AddIdentity<ApplicationUser, IdentityRole<Guid>>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
 
 var app = builder.Build();
 
@@ -16,6 +29,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
